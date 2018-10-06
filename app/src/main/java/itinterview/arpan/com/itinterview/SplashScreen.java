fda48b9e1e46209a7be6c36a7d623864014192d6
Arpanjparasar <arpangeet@gmail.com>
2018-10-06 23:25:35 +0530

    Google Sign In button added

--- app/src/main/java/itinterview/arpan/com/itinterview/SplashScreen.java
+++ app/src/main/java/itinterview/arpan/com/itinterview/SplashScreen.java
@@ -11,27 +11,18 @@ import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 import com.google.android.youtube.player.YouTubePlayer.Provider;
 
-public class SplashScreen extends  YouTubeBaseActivity  implements
-        YouTubePlayer.OnInitializedListener{
-
-    private static final int RECOVERY_DIALOG_REQUEST = 1;
-    YouTubePlayer youTubePlayer;
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-
-        YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
-
-        youTubeView.initialize("AIzaSyA0Y0x0q9NyIr_BGqwWRuGcvTVPby0B2bw", this);
-
-        Thread myThread =new Thread(){
+        Thread myThread = new Thread() {
             @Override
             public void run() {
                 try {
-                    sleep(1000 * 30);
-                    Intent intent =new Intent(getApplicationContext(),login.class);
+                    sleep(4000);
+                    Intent intent = new Intent(getApplicationContext(), login.class);
                     startActivity(intent);
                     finish();
                 } catch (InterruptedException e) {
@@ -41,39 +32,6 @@ public class SplashScreen extends  YouTubeBaseActivity  implements
         };
         myThread.start();
 
+
     }
-
-    @Override
-    public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
-        if (!wasRestored) {
-
-            // loadVideo() will auto play video
-            // Use cueVideo() method, if you don't want to play it automatically
-            player.loadVideo("yeyS23CGGe0");
-
-            // Hiding player controls
-            player.setPlayerStyle(YouTubePlayer.PlayerStyle.CHROMELESS);
-        }
-    }
-
-    @Override
-    public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
-        if (errorReason.isUserRecoverableError()) {
-            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
-        } else {
-            Toast.makeText(this, "error playing", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == RECOVERY_DIALOG_REQUEST) {
-            // Retry initialization if user performed a recovery action
-            getYouTubePlayerProvider().initialize("AIzaSyA0Y0x0q9NyIr_BGqwWRuGcvTVPby0B2bw", this);
-        }
-    }
-
-    private YouTubePlayer.Provider getYouTubePlayerProvider() {
-        return (YouTubePlayerView) findViewById(R.id.youtube_view);
-    }
-}
+};
